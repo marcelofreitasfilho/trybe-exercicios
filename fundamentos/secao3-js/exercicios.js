7cbcd6209a8b76d99e29383a7ce0d17cf1005886
marcelofreitasfilho <marcelodefreitasfilho03@gmail.com>
2023-03-30 14:58:57 -0300

    exercicios

--- fundamentos/secao3-js/exercicios.js
+++ fundamentos/secao3-js/exercicios.js
@@ -44,15 +44,16 @@
 //     console.log("zero")
 //  }
 
-let ang1 = 91;
-let ang2 = 45;
-let ang3 = 45;
-if(ang1 + ang2 + ang3 === 180){
-    console.log(true)
-}
- else if(ang1 + ang2 + ang3 === !180){
-    console.log(false)
- }
- else{
-    console.log("erro")
- }
+// let ang1 = 90;
+// let ang2 = 45;
+// let ang3 = 45;
+// if(ang1 + ang2 + ang3 === 180){
+//     console.log(true)
+// }
+//  else if(ang1 + ang2 + ang3 === !180){
+//     console.log(false)
+//  }
+//  else{
+//     console.log("erro")
+//  }
+
