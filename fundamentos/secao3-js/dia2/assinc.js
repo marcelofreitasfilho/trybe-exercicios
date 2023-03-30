2132c2da246885e9995a6091158027e9e99e70c1
marcelofreitasfilho <marcelodefreitasfilho03@gmail.com>
2023-03-30 14:41:54 -0300

    exercicios pos aula

---
+++ fundamentos/secao3-js/dia2/assinc.js
@@ -0,0 +1,38 @@
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu[1];
+// console.log(menuServices);
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let indexOfPortfolio = menu.indexOf('Portfólio');
+// console.log(indexOfPortfolio);
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// menu.push('Contato')
+// console.log(menu);
+
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+// for (let indice = 0; indice < groceryList.length; indice +=1) {
+//     console.log(groceryList[indice])
+// }
+
+// let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+// for (let nomes of names){
+//     console.log(nomes)
+// }
+
+//  let pessoas= ["amanda", "bruna", "carla"];
+//  let pessoaescolhida = pessoas[2]
+// console.log(pessoaescolhida)
+
+// let jogos = ['Doom', 'Skyrim', 'Final Fantasy'];
+// jogos[0] = 'Warcraft III';
+// console.log(jogos)
+
+// let n = 9;
+
+// let resultado = 100;
+// for (let index = 0; index <= n; index += 1) {
+//   resultado -= index;
+// };
+// console.log(resultado);
+
